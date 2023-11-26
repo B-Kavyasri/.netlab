3843a567a3e97c1b4fb3c6a8802dffb604d8e937
B-Kavyasri <141469186+B-Kavyasri@users.noreply.github.com>
2023-11-26 20:06:53 +0530

    Add files via upload

---
+++ Home(cookies).aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class Home_cookies_ : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String str1 = Request.Cookies["MyCookie"].Value;
+            Response.Write(str1);
+        }
+    }
+}
